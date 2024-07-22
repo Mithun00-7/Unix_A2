d8ff50a6d0d336818c0ecef33aa94328602e853d
Marques <70099318+Mithun00-7@users.noreply.github.com>
2024-07-22 14:19:07 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,13 +1,42 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+
+struct posix_option {
+    const char *name;
+    int option;
+} options[] = {
+    {"_SC_ARG_MAX", _SC_ARG_MAX},
+    {"_SC_CHILD_MAX", _SC_CHILD_MAX},
+    {"_SC_HOST_NAME_MAX", _SC_HOST_NAME_MAX},
+    {"_SC_LOGIN_NAME_MAX", _SC_LOGIN_NAME_MAX},
+    {"_SC_NGROUPS_MAX", _SC_NGROUPS_MAX},
+    {"_SC_OPEN_MAX", _SC_OPEN_MAX},
+    {"_SC_PAGESIZE", _SC_PAGESIZE},
+    {"_SC_RE_DUP_MAX", _SC_RE_DUP_MAX},
+    {"_SC_STREAM_MAX", _SC_STREAM_MAX},
+    {"_SC_TZNAME_MAX", _SC_TZNAME_MAX},
+    {"_SC_VERSION", _SC_VERSION},
+    {"_SC_2_VERSION", _SC_2_VERSION},
+    {"_SC_2_C_BIND", _SC_2_C_BIND},
+    {"_SC_2_C_DEV", _SC_2_C_DEV},
+    {"_SC_2_SW_DEV", _SC_2_SW_DEV},
+    {"_SC_2_LOCALEDEF", _SC_2_LOCALEDEF}
+};
 
 int main() {
-    #ifdef _POSIX_JOB_CONTROL
-        printf("System supports job control.\n");
-    #else
-        printf("System does not support job control.\n");
-    #endif
-    // Add other POSIX feature tests as needed
+    for (size_t i = 0; i < sizeof(options) / sizeof(options[0]); ++i) {
+        errno = 0;  // Reset errno before each call
+        long value = sysconf(options[i].option);
+        if (value == -1) {
+            if (errno == EINVAL) {
+                printf("%s is not supported.\n", options[i].name);
+            } else {
+                perror("sysconf");
+            }
+        } else {
+            printf("%s = %ld\n", options[i].name, value);
+        }
+    }
     return 0;
 }
 
